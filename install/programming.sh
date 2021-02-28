932e2f4bab516920969296bac4212f25600e2696
rodrigo ramos <rodrigo.ramos.santos@gmail.com>
2021-02-28 01:05:27 -0300

    more programming tools

--- install/programming.sh
+++ install/programming.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "---- Installing programming tools ----"
-sudo apt -y install git neovim build-essential
+sudo apt -y install git neovim build-essential nodejs npm clangd gdb golang
 echo "---- Installation completed ----"
 
