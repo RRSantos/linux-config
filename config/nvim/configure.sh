bcfb382ecd73b8137da440ba135f6dfbf5a82f2c
rodrigo ramos <rodrigo.ramos.santos@gmail.com>
2021-03-21 09:14:51 -0300

    copy *.vim files

--- config/nvim/configure.sh
+++ config/nvim/configure.sh
@@ -3,7 +3,7 @@
 echo "---- Configuring nvim ----"
 mkdir -p ~/.config/nvim/autoload
 
-cp init.vim ~/.config/nvim/
+cp *.vim ~/.config/nvim/
 
 wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -O ~/.config/nvim/autoload/plug.vim
 echo "---- Configuration finished ----"
