1541025776d0267b671e8f96f18be645dcdc9cb1
rodrigo ramos <rodrigo.ramos.santos@gmail.com>
2021-03-20 21:33:35 -0300

    lib-sfml and libgdiplus

--- install/programming.sh
+++ install/programming.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "---- Installing programming tools ----"
-sudo apt -y install git neovim build-essential nodejs npm clangd gdb golang
+sudo apt -y install git neovim build-essential libsfml-dev libgdiplus 
+#sudo apt -y install git nodejs npm clangd gdb golang
 echo "---- Installation completed ----"
 
